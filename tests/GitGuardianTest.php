4fbff3da8eb268b26a7a141c2973cf881f9d7ea4
Giovanni Gioffreda <giovanni.gioffreda@gmail.com>
2016-06-20 21:33:51 +1200

    Add more tests.

--- tests/GitGuardianTest.php
+++ tests/GitGuardianTest.php
@@ -4,6 +4,8 @@ namespace Gioffreda\Component\GitGuardian\Tests;
 
 use Gioffreda\Component\GitGuardian\Adapter\BitBucketRemote;
 use Gioffreda\Component\GitGuardian\Adapter\BitBucketRepository;
+use Gioffreda\Component\GitGuardian\Adapter\GitHubRemote;
+use Gioffreda\Component\GitGuardian\Adapter\GitHubRepository;
 use Gioffreda\Component\GitGuardian\Event\GitRepositoryEvent;
 use Gioffreda\Component\GitGuardian\GitGuardian;
 use League\Event\Emitter;
@@ -21,6 +23,26 @@ class GitGuardianTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($emitter === $guardian->getEmitter());
     }
 
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function textCrossExceptionGHBB()
+    {
+        $remote = new GitHubRemote();
+        $repository = new BitBucketRepository('test/test', 'a test', 'https://bitbucket.org/test/test.git');
+        $repository->setRemote($remote);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testCrossExceptionBBGH()
+    {
+        $remote = new BitBucketRemote();
+        $repository = new GitHubRepository('test/test', 'a test', 'https://github.com/test/test.git');
+        $repository->setRemote($remote);
+    }
+
     public function testDefaultOptions()
     {
         $guardian = new GitGuardian();
@@ -28,12 +50,15 @@ class GitGuardianTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('.git-guardian.clone.config.json', $options['clone_config']);
     }
 
-    public function testRemotes()
+    /**
+     * @dataProvider remoteClassesProvider
+     */
+    public function testRemotes($class)
     {
         $guardian = new GitGuardian();
         $this->assertEmpty($guardian->getRemotes());
 
-        $remote = $this->getMockBuilder('Gioffreda\Component\GitGuardian\Adapter\BitBucketRemote')
+        $remote = $this->getMockBuilder($class)
             ->disableOriginalConstructor()
             ->getMock();
 
@@ -42,7 +67,47 @@ class GitGuardianTest extends \PHPUnit_Framework_TestCase
         $this->assertCount(1, $guardian->getRemotes());
     }
     
-    public function testCloneRepository()
+    public function testBitBucketAuthentication()
+    {
+        $remote = new BitBucketRemote();
+        $this->assertEquals([], $remote->getToken());
+        $remote->setOptions(['client_id' => 'clientid', 'client_secret' => 'clientsecret']);
+
+        $body = $this->getMockBuilder('GuzzleHttp\Psr7\Stream')->disableOriginalConstructor()->getMock();
+        $body->expects($this->once())->method('getContents')->willReturn(json_encode($token = [
+            'access_token' => 'testaccesstoken',
+            'expires_in' => 3600
+        ]));
+
+        $tokenResponse = $this->getMockBuilder('GuzzleHttp\Psr7\Response')->disableOriginalConstructor()->getMock();
+        $tokenResponse->expects($this->once())->method('getStatusCode')->willReturn(200);
+        $tokenResponse->expects($this->once())->method('getBody')->willReturn($body);
+
+        $client = $this->getMockBuilder('GuzzleHttp\Client')->getMock();
+        $client->expects($this->once())->method('__call')->willReturn($tokenResponse);
+
+        $remote->setClient($client);
+        $token['expires_at'] =  new \DateTime('+1 hour');
+        $this->assertEquals($token, $remote->getToken());
+    }
+
+    public function testGitHubAuthentication()
+    {
+        $remote = new GitHubRemote();
+        $this->assertEquals([], $remote->getAuthenticationPart());
+        $remote->setOptions(['personal_token' => 'testpersonaltoken', 'username' => 'testusername']);
+        $this->assertEquals(['auth' => ['testusername', 'testpersonaltoken']], $remote->getAuthenticationPart());
+
+        $repository = new GitHubRepository('text/test', 'just a test', $anon = 'https://github.com/test/test.git');
+        $repository->setRemote($remote);
+        $this->assertEquals($anon, $repository->getAnonymousUri());
+        $this->assertEquals('https://testusername:testpersonaltoken@github.com/test/test.git', $repository->getUri());
+    }
+
+    /**
+     * @dataProvider repositoryClassesProvider
+     */
+    public function testCloneRepository($class)
     {
         $guardian = new GitGuardian();
         $destination = sys_get_temp_dir().DIRECTORY_SEPARATOR.uniqid('test-ggioffreda-git-guardian', true);
@@ -65,7 +130,7 @@ class GitGuardianTest extends \PHPUnit_Framework_TestCase
             $this->assertStringStartsWith($destination, $event->getGit()->getPath());
         });
 
-        $repository = $this->getMockBuilder('Gioffreda\Component\GitGuardian\Adapter\BitBucketRepository')
+        $repository = $this->getMockBuilder($class)
             ->disableOriginalConstructor()
             ->getMock();
         $repository->expects($this->any())->method('getName')->willReturn('test/test');
@@ -79,5 +144,21 @@ class GitGuardianTest extends \PHPUnit_Framework_TestCase
         $this->assertContains('git_guardian.pre_clone_repository', $events);
         $this->assertContains('git_guardian.post_clone_repository', $events);
         $this->assertContains('git_guardian.pre_config_log_repository', $events);
+    }
+
+    public function repositoryClassesProvider()
+    {
+        return [
+            ['Gioffreda\Component\GitGuardian\Adapter\BitBucketRepository'],
+            ['Gioffreda\Component\GitGuardian\Adapter\GitHubRepository']
+        ];
+    }
+
+    public function remoteClassesProvider()
+    {
+        return [
+            ['Gioffreda\Component\GitGuardian\Adapter\BitBucketRemote'],
+            ['Gioffreda\Component\GitGuardian\Adapter\GitHubRemote']
+        ];
     }
 }
